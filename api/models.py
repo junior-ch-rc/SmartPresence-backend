4fb696bbf160dc5454d18dfacfaa71526b10168d
Manoel <manoelricardo38@gmail.com>
2025-06-18 13:33:38 -0300

    django project init

---
+++ api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Student(models.Model):
+    id = models.CharField(max_length=20, primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
